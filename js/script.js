697b208e28292eb4170b6f7168c1776c77665456
BabymanStudios <lukepeters771@gmail.com>
2025-04-03 23:50:16 -0500

    File name issue?

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (signupLink && signupForm) {
     signupLink.addEventListener("click", function (e) {
+        e.preventDefault();
         signupForm.classList.add("glow");
         signupForm.scrollIntoView({ behavior: "smooth", block: "center" });
         setTimeout(() => signupForm.classList.remove("glow"), 1500);
