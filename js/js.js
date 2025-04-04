d2b371581a51137d7e674286badd741e7d106026
BabymanStudios <lukepeters771@gmail.com>
2025-04-03 23:43:43 -0500

    Async issue?

--- js/js.js
+++ js/js.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("File linked");
-  
   const signupLink = document.getElementById("signupLink");
   const signupForm = document.getElementById("signupForm");
 
   if (signupLink && signupForm) {
     signupLink.addEventListener("click", function (e) {
-        e.preventDefault();
         signupForm.classList.add("glow");
         signupForm.scrollIntoView({ behavior: "smooth", block: "center" });
         setTimeout(() => signupForm.classList.remove("glow"), 1500);
     });
   }
 });
+
+console.log("File linked");
